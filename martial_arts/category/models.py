eb6baf8be47f76f40c3fec84a0a19f20d67467d7
moses-maker <mosesomoto@gmail.com>
2022-09-09 17:51:39 +0300

    logins

--- martial_arts/category/models.py
+++ martial_arts/category/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+
+class Athlete(models.Model):
+    class Martial_Arts_Category(models.TextChoices):
+        JUDO = 'JU', _('Judo')
+        JIU_JITSU = 'JS', _()
+
+
+
+    martial_arts_category = [
+
+    ]
+    name = models.CharField(max_length=250)
+    category = models.ChoiceField()
+    weight = models.TextField()
+    hours = models.DateTimeField()
+
+
+
--- martial_arts/category/views.py
+++ martial_arts/category/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+
+'''
+def about(request):
+    return render(request)
+'''
